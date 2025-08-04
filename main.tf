cbb66af973b2696bbd6754f657a50d08f068d806
ved727272 <brijesh.zte@gmail.com>
2025-08-04 12:25:13 +0530

    backend1

---
+++ main.tf
@@ -0,0 +1,6 @@
+resource "azurerm_resource_group" "rg" {
+    name = "ramanrg1"
+    location = "westus"
+      
+    }
+  
---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.38.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "c69268aa-c88b-494c-aa80-775984104d2a"
+}
