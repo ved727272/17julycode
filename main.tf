a0c052c9d3ca8258c8fd1305248d45a0abaf1ccf
ved727272 <brijesh.zte@gmail.com>
2025-08-12 17:33:22 +0530

    5th rssfdg

--- main.tf
+++ main.tf
@@ -1,31 +1,6 @@
-resource "azurerm_resource_group" "rg7" {
-    name = "ramanrg7"
+resource "azurerm_resource_group" "rg14" {
+    name = "ramanrg14"
     location = "westus"
       
     }
   
-resource "azurerm_resource_group" "rg8" {
-    name = "ramanrg8"
-    location = "westus"
-      
-    }
-
-
-resource "azurerm_resource_group" "rg10" {
-    name = "ramanrg10"
-    location = "westus"
-      
-    }
-
-resource "azurerm_resource_group" "rg11" {
-    name = "ramanrg11"
-    location = "westus"
-      
-    }
-
-
-    resource "azurerm_resource_group" "rg13" {
-    name = "ramanrg13"
-    location = "westus"
-      
-    }
