1700230dfa12dd2e618ed1800c47f74609ff4e0b
ved727272 <brijesh.zte@gmail.com>
2025-08-12 10:28:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,4 +9,10 @@ resource "azurerm_resource_group" "rg8" {
     location = "westus"
       
     }
-  
+
+
+resource "azurerm_resource_group" "rg9" {
+    name = "ramanrg9"
+    location = "westus"
+      
+    }
